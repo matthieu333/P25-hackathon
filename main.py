ab286e054f63559f4f5131b1984a5d3c9de05878
gaetan-bv2005 <gaetanbv2005@gmail.com>
2026-01-30 10:11:23 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-
-
-try:
-    with open("C:\\Users\\gaeta\\Documents\\MINES\\Nouveau dossier\\port-energy-sim\\P25-hackathon\\sujet-9-clients.csv", "r", encoding="utf-8") as fichier:
-        lignes = fichier.readlines()
-        for ligne in lignes:
-            print(ligne.strip())
+fichier = open("C:\\Users\\gaeta\\Documents\\MINES\\Nouveau dossier\\port-energy-sim\\P25-hackathon\\sujet-9-clients.csv", "r", encoding="utf-8")
+lignes = fichier.readlines()
+for ligne in lignes:
+    print(ligne.strip())
 
 
