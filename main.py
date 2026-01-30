21321288cd4a46de96a7f3d133bfbed949ce6165
Edouard Gardy <edouard.gardy@gmail.com>
2026-01-30 10:29:37 +0100

    On lit le fichier et on créer un dictionnaire par client

--- main.py
+++ main.py
@@ -6,3 +6,16 @@ def lecture(filename):
 
 print(lecture("sujet-9-clients.csv"))
 
+def dico(filename):
+    #créer une liste de dictionnaires : un dictionnaire par client
+    sample = lecture(filename)
+    liste_dicos = []
+    entetes = sample[0].split(",")
+    for ligne in sample[1:]:
+        valeurs = ligne.split(",")
+        dico_ligne = {}
+        for i, entete in enumerate(entetes):
+            dico_ligne[entete] = valeurs[i]
+        liste_dicos.append(dico_ligne)
+    return liste_dicos
+print(dico("sujet-9-clients.csv"))
